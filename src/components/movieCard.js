336c69541ab6f20009ce18f6798d1532aebd54a0
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-02-23 21:19:26 +0800

    implemented movie card component to display movies

---
+++ src/components/movieCard.js
@@ -0,0 +1,60 @@
+import {
+  Card,
+  CardContent,
+  Chip,
+  CardHeader,
+  CardMedia,
+  Avatar,
+  Stack,
+  Box,
+} from "@mui/material";
+
+const MovieCard = ({ movieData }) => {
+  const { name, genre, actor } = movieData;
+
+  return (
+    <Card sx={{ width: 450, height: 750, p: 1 }}>
+      <Box
+        sx={{
+          bgcolor: "#263238",
+        }}
+      >
+        <CardHeader
+          title={name}
+          titleTypographyProps={{
+            align: "center",
+            fontWeight: "medium",
+            fontSize: "h4.fontSize",
+            color: "white",
+          }}
+        />
+        <CardContent>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            spacing={2}
+          >
+            <Chip label={genre} color="info" sx={{ fontSize: "h5.fontSize" }} />
+            <Chip
+              label={actor.name}
+              avatar={
+                <Avatar src={`/avatar/${actor.name.replace(" ", "")}.png`} />
+              }
+              color="primary"
+              sx={{ fontSize: "h5.fontSize" }}
+            />
+          </Stack>
+        </CardContent>
+      </Box>
+
+      <CardMedia
+        component="img"
+        sx={{ height: "auto" }}
+        image={`/moviePosters/${name.replace(" ", "")}.jpg`}
+      />
+    </Card>
+  );
+};
+
+export default MovieCard;
--- src/pages/moviesList.js
+++ src/pages/moviesList.js
@@ -1,6 +1,7 @@
 import { GET_MOVIES } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
 import MovieCard from "../components/movieCard";
+import { Grid } from "@mui/material";
 
 const Movies = () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
@@ -8,7 +9,11 @@ const Movies = () => {
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
   console.log(data);
-  return data.movies.map((movie, index) => <MovieCard name={movie.name} />);
+  return data.movies.map((movie, index) => (
+    <Grid item key={index}>
+      <MovieCard movieData={movie} />
+    </Grid>
+  ));
 };
 
 export default Movies;
