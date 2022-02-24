8051d7a2deeec9f1caa33aec44973e640814ad77
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-02-24 16:22:57 +0800

    added loading spinner

--- src/pages/actorsList.js
+++ src/pages/actorsList.js
@@ -1,13 +1,21 @@
 import { GET_ACTORS } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
-import { Grid } from "@mui/material";
+import { Grid, Box, CircularProgress } from "@mui/material";
 import ActorCard from "../components/actorCard";
 
 const Actors = () => {
   const { loading, error, data } = useQuery(GET_ACTORS);
 
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
+  if (loading)
+    return (
+      <Box sx={{ display: "flex", my: 6 }}>
+        <CircularProgress />
+      </Box>
+    );
+  if (error)
+    return (
+      <Box sx={{ display: "flex", my: 6 }}> `Error! ${error.message}` </Box>
+    );
   console.log(data.actors);
   return data.actors.map((actor, index) => (
     <Grid item key={index}>
--- src/pages/moviesList.js
+++ src/pages/moviesList.js
@@ -1,13 +1,21 @@
 import { GET_MOVIES } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
 import MovieCard from "../components/movieCard";
-import { Grid } from "@mui/material";
+import { Grid, Box, CircularProgress } from "@mui/material";
 
 const Movies = () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
 
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
+  if (loading)
+    return (
+      <Box sx={{ display: "flex", my: 6 }}>
+        <CircularProgress />
+      </Box>
+    );
+  if (error)
+    return (
+      <Box sx={{ display: "flex", my: 6 }}> `Error! ${error.message}` </Box>
+    );
   console.log(data);
   return data.movies.map((movie, index) => (
     <Grid item key={index}>
