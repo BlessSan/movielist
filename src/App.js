67347f5678d2b272542d1c05a0045fc3a6d100f9
BlessSan <87181251+BlessSan@users.noreply.github.com>
2022-02-24 15:12:30 +0800

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import "./App.css";
 import Movies from "./pages/moviesList";
 import Actors from "./pages/actorsList";
 import { Box, Grid } from "@mui/material";
-
+import NavBar from "./components/navbar";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <Grid
-      container
-      spacing={2}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      justifyContent="center"
-    >
-      <Movies />
-    </Grid>
+    <>
+      <NavBar />
+      <Grid
+        container
+        spacing={2}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+        justifyContent="center"
+        sx={{ my: 6 }}
+      >
+        <Outlet />
+      </Grid>
+    </>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,39 @@
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  MenuIcon,
+  Typography,
+  Button,
+  Tabs,
+  Tab,
+  Container,
+} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+
+const NavBar = () => {
+  const location = useLocation();
+  const active = location.pathname;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ backgroundColor: "#263238" }}>
+        <Toolbar>
+          <Container>
+            <Tabs value={active} textColor="white" centered>
+              <Tab label="Movies" value="/" to="/" component={Link} />
+              <Tab
+                label="Actors"
+                value="/actors"
+                to="/actors"
+                component={Link}
+              />
+            </Tabs>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default NavBar;
